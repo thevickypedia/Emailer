2e50f952ff70d8e0ec65920a84e3dc50fde58ef6
thevickypedia <vignesh.sivanandarao@gmail.com>
2023-01-06 22:08:12 -0600

    Set script to use gmail-connector for address validation Fix imports, update requirements.txt and add .gitignore

--- send_email.py
+++ send_email.py
@@ -1,47 +1,51 @@
+import os
 from _curses import error
 from datetime import datetime
-from os import environ, getcwd, path, system
-from re import match
 
+from gmailconnector.validator import validate_email
 from pick import pick
 
 date = datetime.now().strftime("%B %d %Y %I:%M %p")
 
 regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 
-default_id = environ.get('recipient')
-target = input(f'Enter the email address:\t(Hit return to default to {default_id})\n')
+default_id = os.environ.get('recipient')
+prompt = f'Enter the email address:'
 
-if not target:
-    target = default_id
-
-
-def check(email):
-    if not (match(regex, email)):
-        return False
-    return True
+if default_id:
+    prompt += "\t(Hit return to default to {default_id})\n"
+else:
+    prompt += "\n"
 
 
-if not target:
-    exit('Recipient email address is mandatory!!!')
+def get_target():
+    get_target.count += 1
+    if addr := input(prompt):
+        return addr
+    elif default_id:
+        return default_id
+    else:
+        if get_target.count > 2:
+            raise SystemExit("Cannot proceed without a destination email address.")
+        get_target()
 
-if not check(email=target):
-    exit('Invalid email address received.')
 
-default_sub = f"This is the subject line sent at {date}"
-sub = input(f'Enter the subject of the email:\t(Hit return to default to {default_sub})\n')
+get_target.count = 0
+target = get_target()
+result = validate_email(email_address=target, smtp_check=False)
+if result.ok is False:
+    raise SystemExit(result.body)
+
+default_sub = f"Test email from {os.environ.get('USER')} at {date}"
+sub = input(f'Enter the subject of the email:\t(Hit return to default to {default_sub!r})\n')
 if not sub:
     sub = default_sub
 
-body = input('Enter body of the email:\n')
-if not body:
-    body = ""
-
-attachment = input('Enter the name of the attachment file:\t(Hit return for None)\n')
-
+body = input('Enter body of the email:\n') or ""
+attachment = input('Enter the name of path of the attachment file:\t(Hit return for None)\n')
 cmd = None
-if attachment and not path.isfile(attachment):
-    system(f'echo "Skipping attachment as {attachment} is not available at {getcwd()}"\n')
+if attachment and not os.path.isfile(attachment):
+    os.system(f'echo "Skipping attachment as {attachment} is not available at {os.getcwd()}"\n')
 elif attachment:
     title = "Please pick an attachment type: "
     options = [
@@ -52,12 +56,12 @@ elif attachment:
         option, index = pick(options, title, indicator='=>', default_index=0)
     except error:
         index = 1
-    if attachment.endswith('.html') and path.isfile(attachment):
+    if attachment.endswith('.html') and os.path.isfile(attachment):
         if not index:
             cmd = f"echo '{body}' | mail -s '{sub}' --alternative --content-type=text/html --attach={attachment} {target}"  # noqa
         else:
             cmd = f"echo '{body}' | mail -s '{sub}' --content-type=text/html --attach={attachment} {target}"
-    elif path.isfile(attachment):
+    elif os.path.isfile(attachment):
         if not index:
             cmd = f"echo '{body}' | mail -s '{sub}' --alternative --content-type=text/plain --attach={attachment} {target}"  # noqa
         else:
@@ -66,7 +70,7 @@ elif attachment:
 if not cmd:
     cmd = f"echo '{body}' | mail -s '{sub}' {target}"
 
-response = system(cmd)
+response = os.system(cmd)
 
 if response == 0:
     print(f'Email has been sent to {target}.')
